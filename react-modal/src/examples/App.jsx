6cf6a8eddf88c46cd9c51a9ba5001e42ee4e401e
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-05-15 23:25:08 +0000

    update React Modal

---
+++ react-modal/src/examples/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Modal } from '../lib';
+
+function App() {
+  const [ showModal, setShowModal ] = useState(false);
+
+  return (
+    <div>
+      <button onClick={ () => setShowModal(true) }>Open Modal</button>
+      <Modal id="confirmation" isVisible={ showModal } onClose={ () => setShowModal(false) }>Open!</Modal>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-modal/src/lib/components/Modal.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import './Modal.css';
+
+function Modal({ id, isVisible, onClose, children }) {
+  return isVisible && (
+    <div className="bg-modal" onClick={ onClose }>
+      <div id={ id } className="modal">
+        <button className="close-modal" onClick={ onClose }><i className="icon-close"></i></button>
+        { children }
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  id: PropTypes.string.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+};
+
+export default Modal;
