d3a2e0090b78103c685ad2f063d4fdc38325e9c6
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-05-16 03:58:19 +0000

    Update React SelectMenu

--- react-selectmenu/src/lib/components/SelectMenu.jsx
+++ react-selectmenu/src/lib/components/SelectMenu.jsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import './SelectMenu.css';
 
 function SelectMenu({ id, options, onChange }) {
