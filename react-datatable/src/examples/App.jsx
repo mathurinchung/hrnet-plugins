0a36e035d8ee22c79dea948012bf69b9cb464cfc
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-05-29 05:37:02 +0000

    Update react-datatable

--- react-datatable/src/examples/App.jsx
+++ react-datatable/src/examples/App.jsx
@@ -1,8 +1,19 @@
 import { DataTable } from '../lib';
-import columns from './data/columns.json';
+
 
 function App() {
-  const data = [];
+  const columns = [
+    { title: "id", data: "id" },
+    { title: "name", data: "name" }
+  ];
+
+  const data = [
+    { id: "471c0b26-fde2-11ed-be56-0242ac120002", name: "avalon" },
+    { id: "4e728c2e-fde2-11ed-be56-0242ac120002", name: "anundr" },
+    { id: "56751cc0-fde2-11ed-be56-0242ac120002", name: "lorna" },
+    { id: "5c245064-fde2-11ed-be56-0242ac120002", name: "muthoni" },
+    { id: "655b79a0-fde2-11ed-be56-0242ac120002", name: "orietta" }
+  ];
 
   return (
     <div className="App">
